e0e7e6f9f579c373a3aef65c7c9915c6eb11b696
unknown <you@example.com>
2020-10-17 11:56:13 +0700

    'l'

--- components/footer.js
+++ components/footer.js
@@ -17,12 +17,12 @@ export default class Footer extends React.Component {
                 <aside className="footerHolder overflow-hidden bg-lightGray pt-xl-23 pb-xl-8 pt-lg-10 pb-lg-8 pt-md-12 pb-md-8 pt-10">
                 <div className="container">
                     <div className="row">
-                       {/*<div className="col-12 col-sm-6 col-lg-4 mb-lg-0 mb-4">
+                     <div className="col-12 col-sm-6 col-lg-4 mb-lg-0 mb-4">
                             <h3 className="headingVI fwEbold text-uppercase mb-7">Kontak kami</h3>
                             <ul className="list-unstyled footerContactList mb-3">
-                                <li className="mb-3 d-flex flex-nowrap pr-xl-20 pr-0"><span className="icon icon-place mr-3"></span> <address className="fwEbold m-0">Address: London Oxford Street, 012 United Kingdom.</address></li>
-                                <li className="mb-3 d-flex flex-nowrap"><span className="icon icon-phone mr-3"></span> <span className="leftAlign"> <a href="javascript:void(0);">(+032) 3456 7890</a></span></li>
-                                <li className="email d-flex flex-nowrap"><span className="icon icon-email mr-2"></span> <span className="leftAlign"> <a href="javascript:void(0);">Botanicalstore@gmail.com</a></span></li>
+                                <li className="mb-3 d-flex flex-nowrap pr-xl-20 pr-0"><span className="icon icon-place mr-3"></span> <address className="fwEbold m-0">Alamat: Sukamantri, Kec. Tamansari, Bogor, Jawa Barat 16610.</address></li>
+                                <li className="mb-3 d-flex flex-nowrap"><span className="icon icon-phone mr-3"></span> <span className="leftAlign"> +62 899-5940-606</span></li>
+                                <li className="email d-flex flex-nowrap"><span className="icon icon-email mr-2"></span> <span className="leftAlign">zmbpedia@gmail.com</span></li>
                             </ul>
                             <ul className="list-unstyled followSocailNetwork d-flex flex-nowrap">
                                 <li className="fwEbold mr-xl-11 mr-md-8 mr-3">Ikuti kami:</li>
@@ -34,14 +34,13 @@ export default class Footer extends React.Component {
                         <div className="col-12 col-sm-6 col-lg-3 pl-xl-14 mb-lg-0 mb-4">
                             <h3 className="headingVI fwEbold text-uppercase mb-6">Informasi</h3>
                             <ul className="list-unstyled footerNavList">
-                                <li className="mb-1"><a href="javascript:void(0);">New Products</a></li>
-                                <li className="mb-2"><a href="javascript:void(0);">Top Sellers</a></li>
-                                <li className="mb-2"><a href="javascript:void(0);">Our Blog</a></li>
-                                <li className="mb-2"><a href="javascript:void(0);">About Our Shop</a></li>
-                                <li><a href="javascript:void(0);">Privacy policy</a></li>
+                                <li className="mb-1"><a href="#">Katalog produk</a></li>
+                                <li className="mb-2"><a href="#">Cara belanja</a></li>
+                                <li className="mb-2"><a href="#">Tentang kami</a></li>
+                
                             </ul>
                         </div>
-                    */}
+             
                        
                     </div>
                 </div>
