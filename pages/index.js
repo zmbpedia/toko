5bf5c9fe3a3ef29d58d14a7ef59554d022463421
unknown <you@example.com>
2020-10-19 08:12:31 +0700

    'l'

--- pages/index.js
+++ pages/index.js
@@ -162,7 +162,7 @@ export default class Home extends React.Component {
 										<span className="price d-block fwEbold">
 											{a.harga}
 										</span>
-{a.stok ==='false' ? <span class="hotOffer green fwEbold text-uppercase text-white position-absolute d-block ml-8">Tersedia</span> : 
+{a.stok ==='true' ? <span class="hotOffer green fwEbold text-uppercase text-white position-absolute d-block ml-8">Tersedia</span> : 
 <span class="hotOffer fwEbold text-uppercase text-white position-absolute d-block ml-8">Stok habis</span>
 }
 
